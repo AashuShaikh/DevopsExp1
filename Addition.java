2cf5ccce1af48112021ab9b74beec953b728a67c
Asad Shaikh <aashushaikh1312@gmail.com>
2025-02-05 19:06:41 +0530

    Changes for exp2

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int ans = add(5, 10);
+        System.out.println("Answer is: " + ans);
+    }
+
+}
